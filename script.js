77648346776e5a2eeeeec5b60a005ded2942598b
saiyasun <124953343+saiyasun@users.noreply.github.com>
2025-02-26 12:07:59 +0800

    updated javascript for contact page

--- script.js
+++ script.js
@@ -6,21 +6,45 @@
         let formClose = document.getElementById('email-close')
         let submitEmail = document.getElementById('email-submit')
         let emailMe = document.querySelector('#email-me h2')
+        let hrLine = document.getElementById('separator');
+        let contactMe = document.getElementById('all-contact')
         // ----------------
 
 // Functions
     // Contact page
         // --Email--
         let showEmailForm = function() {
-            emailIcon.style.visibility = 'hidden';
-            emailMe.style.visibility = 'hidden';
-            emailForm.style.visibility = 'visible';
+            if (window.matchMedia("(max-width: 768px)").matches) {
+                hrLine.style.visibility = 'hidden';
+                contactMe.style.visibility = 'hidden';
+                emailIcon.style.visibility = 'hidden';
+                emailMe.style.visibility = 'hidden';
+                emailForm.style.visibility = 'visible';
+                emailForm.style.zIndex = '1000'; // Bring to the front
+            } else {
+                emailIcon.style.visibility = 'hidden';
+                emailMe.style.visibility = 'hidden';
+                emailForm.style.visibility = 'visible';
+            }
         }
 
         let hideEmailForm = function() {
             emailIcon.style.visibility = 'visible';
             emailMe.style.visibility = 'visible';
             emailForm.style.visibility = 'hidden';
+
+            if (window.matchMedia("(max-width: 768px)").matches) {
+                hrLine.style.visibility = 'visible';  // Use display instead of visibility
+                contactMe.style.visibility = 'visible';  // Use display instead of visibility
+                emailIcon.style.visibility = 'visible';
+                emailMe.style.visibility = 'visible';
+                emailForm.style.visibility = 'hidden';
+                emailForm.style.zIndex = '-1'; 
+            } else {
+                emailIcon.style.visibility = 'visible';
+                emailMe.style.visibility = 'visible';
+                emailForm.style.visibility = 'hidden';
+            }
         }
 
         let emailSubmission = function(event) {
@@ -40,7 +64,7 @@
             let mailtoLink = `mailto:asiah@asiahcrutchfield.com?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(senderMessage)}`;
 
             window.location.href = mailtoLink;
-        }
+        }     
         // ----------------
 
 // Events
