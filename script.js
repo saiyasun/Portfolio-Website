8e0f4f6fce82f6bc8e0aba86f941283d57fa5789
saiyasun <124953343+saiyasun@users.noreply.github.com>
2025-07-12 20:21:58 +0800

    Added javascript functionality to tagline section

---
+++ script.js
@@ -0,0 +1,17 @@
+// || LANDING PAGE ||
+function separator(id, childElements, separator) { // "id", "childElements" and "separator" must be strings
+    console.log("Separator function running..."); 
+    const container = document.getElementById(id);
+    const children = container.querySelectorAll(childElements);
+
+    for (let i = 0; i < children.length-1; i++) {
+        const separation = document.createElement('p'); // or 'p', 'div', etc.
+        separation.textContent = separator;
+        separation.classList.add(`${id}-separator`); // add class name to make it easier to style
+        children[i].after(separation);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    separator("tagline", "p", "✦");
+});
