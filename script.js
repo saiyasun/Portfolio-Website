7fa706dfa488d50596f25b5b81654fa40d6c928b
saiyasun <124953343+saiyasun@users.noreply.github.com>
2025-07-26 12:44:56 +0800

    Create function to switch names upon translation

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // || HERO SECTION || 
-function switchNames () { // switch names when changing languages
-    
+function switchNames (lang) { // switch names when changing languages
+    const enName = document.getElementById('hero_en-name');
+    const zhName = document.getElementById('hero_zh-name');
+    const langName = lang.toLowerCase();
+
+    if (langName == 'en' || langName == 'eng' || langName == 'english') {
+        enName.style.zIndex = 1;
+        zhName.style.zIndex = 0;
+
+        enName.style.color = 'black';
+        zhName.style.color = 'white';
+            zhName.style.mixBlendMode = 'overlay';
+            enName.style.mixBlendMode = 'normal';
+
+        zhName.style.opacity = 0.5;
+        enName.style.opacity = 1;
+    } else if (langName == 'zh' || langName == 'chinese') {
+        zhName.style.zIndex = 1;
+        enName.style.zIndex = 0;
+
+        zhName.style.color = 'black';
+        enName.style.color = 'white';
+            enName.style.mixBlendMode = 'overlay';
+            zhName.style.mixBlendMode = 'normal';
+
+        enName.style.opacity = 0.5;
+        zhName.style.opacity = 1;
+    }
 }
 // ||   || 
