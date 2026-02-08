6fea5584a6621818773878794fad465614b357f6
saiyasun <124953343+saiyasun@users.noreply.github.com>
2026-02-09 02:57:39 +0800

    Create function to control navbar active states

--- script.js
+++ script.js
@@ -6,11 +6,39 @@ let enName = document.querySelector("#en-name").textContent;
     if (enName.includes(zhName)) {
         enName = enName.replace(zhName, "").trim()
     }
-console.log(enName)
+function activeSelect(navbar) {
+    const active = "nav_active";
+
+    navbar.forEach(link => {
+        link.addEventListener("click", () => {
+            // 1. remove active class from all
+            navbar.forEach(l => {
+                l.classList.remove(active)
+            });
+        link.classList.add(active)
+        });
+    });
+}
 
 // !Hero!
+const mainNav = document.querySelectorAll("#main-nav a:not([data-static])")
+const mobileNav = document.querySelectorAll(".nav-rail a")
+
+// make clicked link active
+activeSelect(mainNav);
+activeSelect(mobileNav);
 
 // 👨🏾About👨🏾
+const aboutSection = document.getElementById("about-container") // container for all subsections
+const aboutNav = document.querySelectorAll("#bio-navigation a")
+const aboutSubSections = document.querySelectorAll(".about-subsection")
+
+activeSelect(aboutNav); // make clicked link active
+aboutNav.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+    })
+})
 
 // 🤹Skills🤹
 
