2e9c5a3d77a007b972f7a2a6a3220c3959e0d362
saiyasun <124953343+saiyasun@users.noreply.github.com>
2026-02-10 17:18:28 +0800

    Create functions to populate template sections

--- script.js
+++ script.js
@@ -38,11 +38,28 @@ function toggleSection(nav, div) {
     })
 }
 
-// clones template
-function mkTemplate(template, container) {
-    const content = template.content.cloneNode(true)
-    container.appendChild(content)
+// hides section if json array has no content
+function ifEmpty(container, dataArray) {
+    if (!dataArray || dataArray.length === 0) {
+        container.classList.add(hideClass);
+        return true;
+    }
+    container.classList.remove(hideClass);
+    return false
 }
+
+// ~file paths~
+const templateContent = "content/temp_content.json"
+const translationContent = "content/translations.json"
+
+// fetch content
+async function fetchContent(filePath) {
+    const response = await fetch(filePath);
+    const data = await response.json()
+
+    return data
+}
+
 
 // •nav•
 const mainNav = document.querySelectorAll("#main-nav a:not([data-static])")
@@ -70,6 +87,26 @@ const fluencyLevels = ["beginner", "intermediate", "advanced", "native"]
 // !~language template~!
 const langTemplate = document.getElementById("lang_template")
 const langContainer = document.getElementById("languages")
+const langSection = document.getElementById('hLanguages')
+
+async function buildLanguages() {
+    const langData = await fetchContent(templateContent)
+    const languages = langData.skills.languages
+
+    languages.forEach(lang => {
+        const clone = langTemplate.content.cloneNode(true);
+
+        const langName = clone.querySelector(".lang-name")
+        const langFlag = clone.querySelector(".lang-flag")
+        const langFluency = clone.querySelector(".fluency")
+
+        langName.textContent = lang.lang
+        langFlag.src = `images/icons/${lang.flag}`
+        langFluency.textContent = fluencyLevels[lang.fluency]
+
+        langContainer.append(clone)
+    })
+}
 
 // ~tech~
 const techNav = document.querySelectorAll("#tech-nav a")
@@ -83,37 +120,165 @@ toggleSection(techNav, techSubSections);
 // !~tech template~!
 const techTemplate = document.getElementById("tech_template")
 const techContainerU = document.getElementById("tech_stack-u") // unordered
-const techContainerO = document.querySelectorAll(".tech_cat-container") // ordered
+const techContainerO = document.querySelectorAll(".tech_stack-o") // ordered
+    const techCategoryMap = {}
+    techContainerO.forEach(list => {
+        const cat = list.dataset.tech_cat; // "frotend", "backend", etc (eg: frontend: <ul data-tech_cat="frontend"></ul>)
+        techCategoryMap[cat] = list
+    })
+const techSection = document.getElementById("technologies")
+
+async function buildTech() {
+    const techData = await fetchContent(templateContent)
+    const technology = techData.skills.technologies
+
+    technology.forEach(tech => {
+        const cloneU = techTemplate.content.cloneNode(true);
+
+        const techName = cloneU.querySelector(".tech-name")
+        const techImg = cloneU.querySelector(".tech-img")
+
+        techName.textContent = tech.name
+        techImg.src = `images/icons/tech/${tech.img}`
+
+        // always append to unordered list
+        techContainerU.append(cloneU)
+        
+        // add to ordered list
+        const cloneO = techTemplate.content.cloneNode(true);
+
+        cloneO.querySelector(".tech-name").textContent = tech.name
+        cloneO.querySelector(".tech-img").src = `images/icons/tech/${tech.img}`
+
+        const categoryName = techCats[tech.category]
+        const targetList = techCategoryMap[categoryName]
+
+        if (targetList) {
+            targetList.appendChild(cloneO)
+        }
+    })
+}
 
 // !~certs template~!
 const certTemplate = document.getElementById("cert_template")
 const certContainer = document.getElementById("cert-container")
+const certSection = document.getElementById("certifications")
+
+async function buildCerts() {
+    const certData = await fetchContent(templateContent)
+    const certifications = certData.skills.certifications
+
+    certifications.forEach((cert, index) => {
+        const clone = certTemplate.content.cloneNode(true);
+        const cloneEl = clone.querySelector(".cert")
+        if (index % 2 === 1) {
+            cloneEl.classList.add("cert-reverse")
+        }
+
+        const certName = clone.querySelector(".cert-name")
+        const certDate = clone.querySelector(".cert-date")
+
+        certName.textContent = cert.name
+        certDate.textContent = cert.year != 0 ? cert.year : "in progress"
+        
+        certContainer.append(clone)
+    })
+}
 
 // 🛠️Projects🛠️
 // !~projects template~!
 const projTemplate = document.getElementById("project_template")
 const projContainer = document.getElementById("projects-container")
+const expSection = document.getElementById('experience')
 
 // 🎓👷Experience/Education🎓👷
 const dutyBtn = document.querySelectorAll(".duties-btn")
 const dutiesList = document.querySelectorAll(".duties-list")
 
-dutyBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        const list = btn.nextElementSibling; // <ul> right after the button
-        if (!list || !list.classList.contains("duties-list")) return;
+expSection.addEventListener("click", (e) => {
+    const btn = e.target.closest(".duties-btn")
+    if (!btn) return
 
-        list.classList.toggle(hideClass)
-    })
+    const list = btn.nextElementSibling;
+
+    if (!list || !list.classList.contains("duties-list")) return;
+
+    list.classList.toggle(hideClass)
 })
 
 // !~experience template~!
-const expjTemplate = document.getElementById("exp_template")
+const expTemplate = document.getElementById("exp_template")
 const expContainer = document.getElementById("exp-container")
 const expReverseClass = "exp-reverse"
+
+async function buildExperience() {
+    const expData = await fetchContent(templateContent)
+    const experience = expData.exp_edu.experience
+
+    experience.forEach((exp, index) => {
+        const clone = expTemplate.content.cloneNode(true)
+        const cloneEl = clone.querySelector(".exp")
+        if (index % 2 === 1) {
+            cloneEl.classList.add("exp-reverse")
+        }
+
+        const expTitle = clone.querySelector(".exp-title")
+        const expStartDate = clone.querySelector(".start-date")
+        const expEndDate = clone.querySelector(".end-date")
+        const expCity = clone.querySelector(".exp_city")
+        const expCountry = clone.querySelector(".exp_country")
+        const expSummary = clone.querySelector(".exp-summary")
+        const expDuties = clone.querySelector(".duties-list")
+
+        expTitle.textContent = exp.title
+        expStartDate.textContent = exp.start_date
+        expEndDate.textContent = exp.end_date == 0 ? "present" : exp.end_date
+        expCity.textContent = exp.city
+        expCountry.textContent = exp.country
+        expSummary.textContent = exp.summary
+        // clear list
+        expDuties.innerHTML = ""
+        const expDutiesList = exp.duties
+        expDutiesList.forEach(duty => {
+            const li = document.createElement('li')
+            li.textContent = duty
+            expDuties.append(li)
+        })
+
+        expContainer.append(clone)
+    })
+}
 
 // !~education template~!
 const eduTemplate = document.getElementById("edu_template")
 const eduContainer = document.getElementById("edu-container")
 
 // ✉️Contact✉️
+
+// function to visualize accessing json content
+async function loadContent(filePath) {
+    const response = await fetch(filePath);
+    const data = await response.json()
+
+    const {languages, technologies, certifications} = data.skills
+    const {experience, education} = data.exp_edu
+
+    if (!ifEmpty(langSection, languages)) {
+        buildLanguages();
+    }
+    
+    if (!ifEmpty(techSection, technologies)) {
+        buildTech();
+    }
+
+    if (!ifEmpty(certSection, certifications)) {
+        buildCerts();
+    }
+
+    if (!ifEmpty(expSection, experience)) {
+        buildExperience();
+    }
+    console.log(data)
+}
+
+loadContent(templateContent)
