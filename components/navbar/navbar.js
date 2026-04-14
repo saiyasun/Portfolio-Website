e86de8605e8e1bf81741c043a9f516167446a045
saiyasun <124953343+saiyasun@users.noreply.github.com>
2026-04-15 00:03:02 +0800

    Add js navbar component

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,12 +1,14 @@
-<!--SITE NAV BAR-->
-    <nav id="main-nav" class="glass-nav" aria-label="Primary navigation">
+class SiteNavbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav id="main-nav" class="glass-nav" aria-label="Primary navigation">
         <ul>
             <li><a class="nav_active" href="/index.html" data-i18n="nav.home">HOME</a></li>
             <li><a href="/index.html#about" data-i18n="nav.about">ABOUT</a></li>
             <li><a href="/index.html#experience" data-i18n="nav.exp">EXPERIENCE</a></li>
             <li><a href="/index.html#skills" data-i18n="nav.skills">SKILLS</a></li>
             <li><a href="/index.html#projects" data-i18n="nav.projects">PROJECTS</a></li>
-            <li><a href="blog/index.html" data-i18n="nav.blog">BLOG</a></li>
+            <li><a href="/blog/index.html" data-i18n="nav.blog">BLOG</a></li>
             <li><a href="/index.html#contact" data-i18n="nav.contact">CONTACT</a></li>
             <hr id="main-nav_hr">
             <li><a href="/docs/Asiah Crutchfield - Resume.pdf" data-static><img class="icon" src="/images/icons/resume-blk.svg" alt="resume"></a></li>
@@ -29,4 +31,8 @@
                 <button class="lang-btn glass-nav" id="lang_btn-zh" data-lang="zh">中</button>
             </div>
         </nav>
+        `
+    }
+}
 
+customElements.define("site-navbar", SiteNavbar)
